48d0c0bc99208aeb70f96c1263844a68727c7d97
Pownyfals <pownyfals@gmail.com>
2023-06-06 00:02:26 +0700

    check salah

--- src/main/java/Driver.java
+++ src/main/java/Driver.java
@@ -2,7 +2,7 @@
 public class Driver {
     public static void main(String[] args) {
         int number = 7; // Ganti bilangan sesuai dengan yang ingin Anda cekkk
-        String result = NumberCheckerr.checkNumber(number);
+        String result = NumberCheckerrr.checkNumber(number);
         System.out.println("Bilangan " + number + " adalah " + result);
     }
 }
